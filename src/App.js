2d71ee76667bb4a063086c17ebaed1ea4473968b
Hoan Le <hoanleecr@gmail.com>
2022-07-13 20:20:05 -0700

    figured out a way to upload img and having it show up on console and debugged the slow rendering page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import {useState, useEffect} from 'react'
 import * as mobilenet from '@tensorflow-models/mobilenet'
+// import axios from 'axios'
 
 function App() {
-  // use the useState as a way to save the model
+  // use the useState as a way to save the mobilenet model in the model state
   const [model,setModel] = useState(null)
+  const [imgUrl, setImgUrl] = useState(null)
+
+  const imageUpload = (e) => {
+    // destructoring the files 
+    const {files} = e.target
+    // what if there is no files? how to check if there is a file
+    if (files.length > 0) {
+      // creates a url for the file that is passed in the parameter on index 0
+      const url = URL.createObjectURL(files[0])
+      // set the image
+      setImgUrl(url)
+    } else {
+      setImgUrl(null)
+    }
+  }
+
   return (
     <div className="App">
-      <h1>happy new year!</h1>
-      {/* able to upload files locally, or if you happen to use the app on the phone, then itll trigger the camera */}
-      <input type='file' accept='image/*' capture='camera'/>
+      <h1>IDENTIFY ME!</h1>
+      {/* able to upload files locally*/}
+      <input
+      onChange={imageUpload}
+       type='file' 
+      //  accept all image files
+      accept='image/*' 
+      // if you're on your phone, it'll trigger the camera
+      capture='camera' />
     </div>
   );
 }
